7aa1bcaff508d13d099061422350c9c6283c7fac
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2020-03-14 17:59:15 +0900

    処理の関数化と表記の変更
    呼び出し側で山札を作り参照渡しで更新する作りに。

--- Main.c
+++ Main.c
@@ -2,74 +2,117 @@
 #include<stdlib.h>
 #include<time.h>
 
+// トランプの役柄
+enum tag_suit {
+    SPADES,
+    HEARTS,
+    DIAMONDOS,
+    CLUBS,
+    SUIT_MAX,
+}SUIT;
+// トランプの強さ
+#define CARDS_RANK_MAX (13)
+
+// 山札の初期化
+void DeckInit(short int* deck,  size_t length);
+// 山札を混ぜる
+void DeckShuffle(short int* deck, size_t length);
+// 山札を引く
+short int DeckDraw(short int* deck, size_t length, short int index);
+// カードの表記
+void PrintCards(short int id);
+
 int main(void)
 {
-    // トランプの定数
-    const int SUIT_MAX = 4;
-    const int CARDS_RANK_MAX = 13;
-    const int DECK_MAX = SUIT_MAX * CARDS_RANK_MAX;
-
     // トランプの配列と値
-    short int deck[DECK_MAX];
-    short int suit, rank;
+    short int deck[SUIT_MAX * CARDS_RANK_MAX];
+    size_t length = sizeof(deck)/sizeof(deck[0]);
     short int index;
-    short int rnd, tmp;
+    short int drawCnt;
 
     // 乱数の初期化
     srand((unsigned int)time(NULL));
 
     // 山札の初期化
-    for (index = 0; index < DECK_MAX; index++)
+    DeckInit(deck, length);
+
+    // 山札を混ぜる
+    DeckShuffle(deck, length);
+
+    // トランプの文字列表記
+    drawCnt = length;
+    while(0 <= --drawCnt)
+    {
+        // トランプの内容を表記
+        printf("%d：",drawCnt+1);
+        PrintCards(DeckDraw(deck,length,drawCnt));
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// 山札の初期化
+void DeckInit(short int* deck, size_t length)
+{
+    short int index = 0;
+    for (index = 0; index < length; index++)
     {
         deck[index] = index+1;
     }
+}
 
-    // 山札を混ぜる
-    for (index = 0; index < DECK_MAX; index++)
+// 山札を混ぜる
+void DeckShuffle(short int* deck, size_t length)
+{
+    short int rnd, tmp;
+    short int index;
+
+    for (index = 0; index < length; index++)
     {
         rnd = rand()%CARDS_RANK_MAX;
         tmp = deck[index];
         deck[index] = deck[rnd];
         deck[rnd] = tmp;
     }
+}
 
-    // トランプの文字列表記
-    for (index = 0; index < DECK_MAX; index++)
+// 山札を引く
+short int DeckDraw(short int* deck, size_t length, short int index)
+{
+    short int result = -1;
+    if (0<=index&&index<length)
     {
-        // トランプの役柄と強さを算出
-        suit = (deck[index]-1)/CARDS_RANK_MAX;
-        rank = (deck[index]%CARDS_RANK_MAX)+1;
+        result = deck[index];
+        deck[index] = -1;
+    }
+    return result;
+}
 
-        // 役柄の表記
-        switch (suit)
-        {
-            case 0: printf("♤ "); break;
-            case 1: printf("♡ "); break;
-            case 2: printf("♢ "); break;
-            case 3: printf("♧ "); break;
-            default: printf("Joker"); break;
-        }
+// カードの表記
+void PrintCards(short int id)
+{
+    // トランプの役柄と強さを算出
+    short int suit = (id-1)/CARDS_RANK_MAX;
+    short int rank = (id%CARDS_RANK_MAX)+1;
 
-        // 強さの表記
-        switch (rank)
-        {
-            case 13: printf("K"); break;
-            case 12: printf("Q"); break;
-            case 11: printf("J"); break;
-            case 1:  printf("A"); break;
-            default: printf("%d", rank); break;
-        }
-
-        // 役柄降順に並べた際の行区切り
-        if (index%CARDS_RANK_MAX==CARDS_RANK_MAX-1)
-        {
-            printf("\n");
-        }
-        else
-        {
-            printf(",");
-        }
+    // 役柄の表記
+    switch (suit)
+    {
+        case 0: printf("♤ "); break;
+        case 1: printf("♡ "); break;
+        case 2: printf("♢ "); break;
+        case 3: printf("♧ "); break;
+        default: printf("Joker"); break;
     }
 
-    return 0;
+    // 強さの表記
+    switch (rank)
+    {
+        case 13: printf("K"); break;
+        case 12: printf("Q"); break;
+        case 11: printf("J"); break;
+        case 1:  printf("A"); break;
+        default: printf("%d", rank); break;
+    }
 }
